0db895e5cb6d226a241a7b3436654444aeaef8f1
ranim125 <ranim.gharbi@etudiant-fst.utm.tn>
2025-12-05 15:43:19 +0100

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,18 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { CategoryList } from './categories/category-list/category-list';
 import { CategoryForm } from './categories/category-form/category-form';
+import { LoginComponent } from './auth/login/login.component';
+import { AuthGuard } from './auth/guards/auth.guard';
+import { RoleGuard } from './auth/guards/role.guard';
+
 
 export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/categories', pathMatch: 'full' },
   { path: 'categories', component: CategoryList },
   { path: 'categories/add', component: CategoryForm },
   { path: 'categories/edit/:id', component: CategoryForm },
   { path: '**', redirectTo: '/categories' }
-];
-=======
-import { LoginComponent } from './auth/login/login.component';
-import { AuthGuard } from './auth/guards/auth.guard';
-import { RoleGuard } from './auth/guards/role.guard';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
 
   {
     path: 'admin',
@@ -34,4 +30,4 @@ export const routes: Routes = [
 
   { path: '**', redirectTo: 'login' }
 ];
->>>>>>> origin/feat/auth
+
